c01cdb0fd02f8d0f828f36adcfcde9353049f8fe
manthan98 <contact@manthanshah.com>
2018-08-25 14:39:13 -0400

    Rollback

---
+++ GetSense-iOS/Model/Clarifai.swift
@@ -0,0 +1,14 @@
+//
+//  ClarifaiObject.swift
+//  GetSense-iOS
+//
+//  Created by Manthan Shah on 2018-08-25.
+//  Copyright © 2018 Manthan Shah. All rights reserved.
+//
+
+import Foundation
+
+struct Clarafai {
+    let modelId: String?
+    let conceptId: String?
+}
